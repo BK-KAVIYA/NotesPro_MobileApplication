a41ea990197fd13704e8633e6d1d0876a4962ab0
Baladurage Kavinda <dilshankavinda371@gmail.com>
2022-12-13 12:11:31 +0530

    Note are Added to database

--- app/src/main/java/com/example/notespro/NoteDetails.java
+++ app/src/main/java/com/example/notespro/NoteDetails.java
@@ -1,11 +1,17 @@
 package com.example.notespro;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentReference;
 
 public class NoteDetails extends AppCompatActivity {
 
@@ -29,8 +35,29 @@ public class NoteDetails extends AppCompatActivity {
                 if(noteTitle==null || noteTitle.isEmpty()){
                     titleEditeText.setError("Title is required");
                     return;
-                }else{
+                }
+                Note note=new Note();
+                note.setTitle(noteTitle);
+                note.setContent(noteContent);
+                note.setTimestamp(Timestamp.now());
 
+                saveNoteToFirebase(note);
+            }
+        });
+    }
+    void saveNoteToFirebase(Note note){
+        DocumentReference documentReference;
+        documentReference=Utility.getCollectionRefferenceForNote().document();
+        documentReference.set(note).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    //note is added
+                    Utility.showToast(NoteDetails.this,"Note Added Successfully");
+                    finish();
+                }else{
+                    //note added fail
+                    Utility.showToast(NoteDetails.this,"Failed to add note");
                 }
             }
         });
