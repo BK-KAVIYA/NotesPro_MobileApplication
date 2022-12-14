80ebf70d3f306ebc9ae12a86103be094e6152e66
Baladurage Kavinda <dilshankavinda371@gmail.com>
2022-12-14 15:06:30 +0530

    Recycle view problem solve

--- app/src/main/java/com/example/notespro/MainActivity.java
+++ app/src/main/java/com/example/notespro/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.notespro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.DownloadManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ListView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.Query;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageButton addNote;
+    RecyclerView recyclerView;
+    ImageButton menueButton;
+    NoteAdapter noteAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +32,42 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this,NoteDetails.class));
             }
         });
+        recyclerView=findViewById(R.id.recycler);
+        menueButton=findViewById(R.id.menu_button);
+        menueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
+        setupRecycleView();
+
+    }
+    void setupRecycleView(){
+        Query query=Utility.getCollectionRefferenceForNote().orderBy("timestamp", Query.Direction.DESCENDING);
+        FirestoreRecyclerOptions<Note> options=new FirestoreRecyclerOptions.Builder<Note>().setQuery(query,Note.class).build();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        noteAdapter=new NoteAdapter(options,this);
+        recyclerView.setAdapter(noteAdapter);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        noteAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        noteAdapter.stopListening();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        noteAdapter.notifyDataSetChanged();
     }
 }
--- app/src/main/java/com/example/notespro/Utility.java
+++ app/src/main/java/com/example/notespro/Utility.java
@@ -1,12 +1,16 @@
 package com.example.notespro;
 
 import android.content.Context;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
+import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
 
 public class Utility {
     static void showToast(Context context,String message){
@@ -16,5 +20,9 @@ public class Utility {
     static CollectionReference getCollectionRefferenceForNote(){
         FirebaseUser currentUser= FirebaseAuth.getInstance().getCurrentUser();
         return FirebaseFirestore.getInstance().collection("notes").document(currentUser.getUid()).collection("my_notes");
+    }
+
+    static String timestampToString(Timestamp timestamp){
+        return  new SimpleDateFormat("MM/dd/yyyy").format(timestamp.toDate());
     }
 }
